92ac4c80188006fe15cb3249e2861cb941c7896e
Mon <tracer.coolkid@gmail.com>
2024-05-16 13:50:07 +0700

    feat:added ex1-6 js

--- ex-3.js
+++ ex-3.js
@@ -2,5 +2,11 @@
 let scores = [100, 20, 3, 1000];
 let minScore;
 // Start coding here
-
+for (let i = 0; i < scores.length; i++){
+    if (i === 0) {
+        minScore = scores[i];
+    } else if (minScore >  scores[i]) {
+        minScore = scores[i];
+    }
+}
 console.log(minScore);
